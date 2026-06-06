b428b4ec626a5aaccb09e09890a63193fd9914e5
samellsaftiee <samell072003@gmail.com>
2026-06-06 15:53:34 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,19 @@
-export { default } from "next-auth/middleware";
+import { NextRequest, NextResponse } from 'next/server'
+import { getToken } from 'next-auth/jwt'
+
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl
+
+  if (pathname.startsWith('/admin') && !pathname.startsWith('/admin/login')) {
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+    if (!token) {
+      return NextResponse.redirect(new URL('/admin/login', req.url))
+    }
+  }
+
+  return NextResponse.next()
+}
 
 export const config = {
-  matcher: ["/admin/dashboard/:path*"],
-};
+  matcher: ['/admin/:path*'],
+}
